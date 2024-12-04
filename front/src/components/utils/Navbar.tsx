eabf57527fb8fd230505ba72d382c36cae0604f5
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-04 09:05:25 +0100

    refactor: Navbar to material ui

--- front/src/components/utils/Navbar.tsx
+++ front/src/components/utils/Navbar.tsx
@@ -1,6 +1,6 @@
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
 import { LoginLogout } from "../login/LoginLogout";
 
 export const Navbar = (_props: unknown) => {
@@ -13,31 +13,40 @@ export const Navbar = (_props: unknown) => {
       navigate(path);
     };
   };
-    return (
-        <Menu pointing secondary>
-        <Menu.Item
-          name="home"
-          active={activeItem === "home"}
-          onClick={createOnClickHandler("/", "home")}
-        ></Menu.Item>
-        <Menu.Item
-          name="display"
-          active={activeItem === "display"}
-          onClick={createOnClickHandler("/display", "display")}
-        ></Menu.Item>
-        <Menu.Item
-          name="form"
-          active={activeItem === "form"}
-          onClick={createOnClickHandler("/form", "form")}
-        ></Menu.Item>
-        <Menu.Item
-          name="shop"
-          active={activeItem === "shop"}
-          onClick={createOnClickHandler("/shop", "shop")}
-        ></Menu.Item>
-        <Menu.Item position="right">
+
+  return (
+    <AppBar position="static" color="default" sx={{ marginBottom: 2 }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button
+            variant={activeItem === "home" ? "outlined" : "text"}
+            onClick={createOnClickHandler("/", "home")}
+          >
+            Home
+          </Button>
+          <Button
+            variant={activeItem === "display" ? "outlined" : "text"}
+            onClick={createOnClickHandler("/display", "display")}
+          >
+            Display
+          </Button>
+          <Button
+            variant={activeItem === "form" ? "outlined" : "text"}
+            onClick={createOnClickHandler("/form", "form")}
+          >
+            Form
+          </Button>
+          <Button
+            variant={activeItem === "shop" ? "outlined" : "text"}
+            onClick={createOnClickHandler("/shop", "shop")}
+          >
+            Shop
+          </Button>
+        </Box>
+        <Box>
           <LoginLogout />
-        </Menu.Item>
-      </Menu>
-    )
-}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
