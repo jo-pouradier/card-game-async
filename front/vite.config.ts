5318d3a5df659ef5c6450080b3e800c5298e0ef5
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-04 11:52:16 +0100

    init vite proxy for api call

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://tp.cpe.fr:8083/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  }
 });
