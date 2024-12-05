c3c824a95e2c675d55ab0150ea81c39914a5f9e5
Pierre-louis <pierre-louis.telep@cpe.fr>
2024-12-05 11:55:18 +0100

    utils not here

--- backend/generation/src/main/java/com/cpe/springboot/utils/JmsListenerConfig.java
+++ backend/generation/src/main/java/com/cpe/springboot/utils/JmsListenerConfig.java
@@ -41,4 +41,5 @@ public class JmsListenerConfig implements JmsListenerConfigurer{
     public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
         registrar.setMessageHandlerMethodFactory(handlerMethodFactory());
     }
+    
 }
