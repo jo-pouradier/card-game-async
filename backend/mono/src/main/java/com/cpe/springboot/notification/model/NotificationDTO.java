859015219d12f814d801cb3e474cb9ca4bc7e26e
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-05 12:09:59 +0100

    create notification service in monolithic
    need to be tested on broker

---
+++ backend/mono/src/main/java/com/cpe/springboot/notification/model/NotificationDTO.java
@@ -0,0 +1,60 @@
+package com.cpe.springboot.notification.model;
+
+public class NotificationDTO {
+    int id;
+    String message;
+    String severity;
+    String sender;
+
+    public NotificationDTO() {
+    }
+
+    public NotificationDTO(int id, String message, String severity, String sender) {
+        this.id = id;
+        this.message = message;
+        this.severity = severity;
+        this.sender = sender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    
+    @Override
+    public String toString() {
+        return "NotificationDTO{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", severity='" + severity + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
+}
