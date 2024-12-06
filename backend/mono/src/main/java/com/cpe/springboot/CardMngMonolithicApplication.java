f61723e938caa51bba237c5ac1ba393939bc5531
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-06 09:32:08 +0100

    test creation topic on queue from java mono

--- backend/mono/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
+++ backend/mono/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
@@ -1,5 +1,7 @@
 package com.cpe.springboot;
 
+import com.cpe.springboot.notification.BrokerSender;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
@@ -14,6 +16,15 @@ import io.swagger.v3.oas.annotations.info.Info;
 public class CardMngMonolithicApplication {
 
 	public static void main(String[] args) {
+		 ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		System.out.println("Sending a notification message.");
+		Sender sender = context.getBean(BrokerSender.class);
+		String queueKey = "fr.cpe.nodejs-app.queue.in";
+		sender.setQueue(context.getEnvironment().getProperty(queueKey));
+		sender.sendMessage(new NotificationDTO(1, "Hello", "INFO", "java monolithic"));
+		sender.init();
+
 		SpringApplication.run(CardMngMonolithicApplication.class, args);
 	}
 
