f722e05819bed66dfbded47e4e4fa23ccdcf353f
Adri1 <adrien.dalbeigue@cpe.fr>
2024-12-06 12:11:52 +0100

    fuse sockets

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,31 +1,17 @@
-import { Provider } from "react-redux";
+import {Provider, useDispatch} from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/utils/Navbar";
 import AppRouter from "./router/AppRouter";
 import { store } from "./store";
 import Notification from "./components/utils/Notification";
 import { useEffect } from 'react';
-import { io } from 'socket.io-client';
+import socketConnection from "./socket/socket.ts";
 
 const App = (_props: unknown) => {
-    useEffect(() => {
-        // Connecter au serveur Socket.IO
-        const socket = io('http://localhost:8080');
-
-        // Écouter une notification
-        socket.on('notification', (message) => {
-            console.log('Notification reçue :', message);
-        });
-
-
-        // Exemple d'envoi d'un événement
-        socket.emit('connected', 'test');
-
-        // Nettoyage à la déconnexion
-        return () => {
-            socket.disconnect();
-        };
-    }, []);
+    const dispatch = useDispatch();
+    useEffect(() =>{
+        socketConnection(dispatch)
+    },[]);
   return (
     <Provider store={store}>
       <BrowserRouter>
