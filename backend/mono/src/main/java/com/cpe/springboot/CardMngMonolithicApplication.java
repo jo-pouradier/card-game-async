4938f19509a82d869e131b94c7d6dac633d59206
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-06 09:32:08 +0100

    test creation topic on queue from java mono

--- backend/mono/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
+++ backend/mono/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
@@ -12,11 +12,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 @EnableJms
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Card Market Rest Api", version = "1.0", description = "Information about the Card Market APi and how to interact with"))
-// doc here localhost:8080/swagger-ui.html
+// doc here localhost:8083/swagger-ui.html
 public class CardMngMonolithicApplication {
 
 	public static void main(String[] args) {
-		 ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
 		System.out.println("Sending a notification message.");
 		Sender sender = context.getBean(BrokerSender.class);
@@ -25,7 +25,7 @@ public class CardMngMonolithicApplication {
 		sender.sendMessage(new NotificationDTO(1, "Hello", "INFO", "java monolithic"));
 		sender.init();
 
-		SpringApplication.run(CardMngMonolithicApplication.class, args);
+		// SpringApplication.run(CardMngMonolithicApplication.class, args);
 	}
 
 
