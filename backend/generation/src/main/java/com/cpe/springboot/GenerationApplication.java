853671805adcea8e9ea7fa888778f9fd25871d80
Pierre-louis <pierre-louis.telep@cpe.fr>
2024-12-06 12:02:05 +0100

    Delete async in generation service

--- backend/generation/src/main/java/com/cpe/springboot/GenerationApplication.java
+++ backend/generation/src/main/java/com/cpe/springboot/GenerationApplication.java
@@ -3,10 +3,7 @@ package com.cpe.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-
 @SpringBootApplication
-@EnableAsync
 public class GenerationApplication {
     public static void main(String[] args) {
         SpringApplication.run(GenerationApplication.class, args);
