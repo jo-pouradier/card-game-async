8db96185850f557063e39d2dfefc2ffa10617c8a
Adrien <adrien.dalbeigue@cpe.fr>
2024-12-08 18:13:35 +0100

    reformat socket

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,26 @@
-import {Provider, useDispatch} from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/utils/Navbar";
 import AppRouter from "./router/AppRouter";
-import { store } from "./store";
+import {store} from "./store";
 import Notification from "./components/utils/Notification";
-import { useEffect } from 'react';
-import socketConnection from "./socket/socket.ts";
+import {useEffect} from 'react';
+import {initSocket} from "./socket/socket.ts";
+
 
 const App = (_props: unknown) => {
-    const dispatch = useDispatch();
-    useEffect(() =>{
-        socketConnection(dispatch)
-    },[]);
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <AppRouter />
-        <Notification />
-      </BrowserRouter>
-    </Provider>
-  );
+    useEffect(() => {
+        initSocket();
+    }, []);
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+                <Notification/>
+            </BrowserRouter>
+        </Provider>
+    );
 };
 
 export default App;
