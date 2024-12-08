f4a89f108a80e4bf4fb396db8d1b38dcf3db9734
jo-pouradier <jocharbo43@gmail.com>
2024-12-08 17:03:40 +0100

    fix login detection

--- front/src/router/LoginProtectedRoutes.tsx
+++ front/src/router/LoginProtectedRoutes.tsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from "react-router";
+import { useAppSelector } from "../hooks";
+import { selectUser } from "../slices/userSlice";
 
 const LoginProtectedRoutes = () => {
-  const isAuth = document.cookie.includes("loggedIn=true");
-  if (!isAuth) {
+  // const isAuth = document.cookie.includes("loggedIn=true");
+  const isAuth = useAppSelector(selectUser)
+  console.debug("isAuth: " + isAuth.id)
+  if (isAuth.id === 0) {
     return <Navigate to="/login?returnTo=shop" />;
   }
   return <Outlet />;
