586e2bea1160f1677fda656998a9bb6c14fca3b1
jo-pouradier <jocharbo43@gmail.com>
2024-12-09 14:13:44 +0100

    rm useless file, format

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,23 @@
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/utils/Navbar";
 import AppRouter from "./router/AppRouter";
-import {store} from "./store";
+import { store } from "./store";
 import Notification from "./components/utils/Notification";
-import {useEffect} from 'react';
-import {initSocket} from "./socket/socket.ts";
-
+import { useEffect } from "react";
+import { initSocket } from "./socket/socket.ts";
 
 const App = (_props: unknown) => {
-    useEffect(() => {
-        initSocket();
-    }, []);
-    return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <Navbar/>
-                <AppRouter/>
-                <Notification/>
-            </BrowserRouter>
-        </Provider>
-    );
+  useEffect(initSocket, []);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+        <Notification />
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
