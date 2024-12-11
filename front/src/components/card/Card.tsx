c1250b1d0207fcccb74cd4f20819f2c826fad0ed
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-11 16:33:58 +0100

    front: better ui for card selection

--- front/src/components/card/Card.tsx
+++ front/src/components/card/Card.tsx
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux";
 import { selectCard } from "../../slices/cardSlice";
 import { CardDisplayLabelEnums } from "../../types/CardDisplayLabelEnums";
 import ICard from "../../types/ICard";
-import { CardShortDisplay } from "./CardShortDisplay";
-import { CardSimpleDisplay } from "./CardSimpleDisplay";
+import CardShortDisplay from "./CardShortDisplay";
+import CardSimpleDisplay from "./CardSimpleDisplay";
 
 export interface CardProps extends ICard {
   display_type: CardDisplayLabelEnums;
@@ -16,9 +16,7 @@ const User = (props: CardProps) => {
     case CardDisplayLabelEnums.SHORT:
       display = (
         <CardShortDisplay
-          image={current_card.image}
-          name={current_card.name}
-          family={current_card.family}
+          {...current_card}
         ></CardShortDisplay>
       );
 
@@ -26,16 +24,7 @@ const User = (props: CardProps) => {
     case CardDisplayLabelEnums.FULL:
       display = (
         <CardSimpleDisplay
-          id={0}
-          name={current_card.name}
-          family={current_card.family}
-          description={current_card.description}
-          image={current_card.image}
-          hp={current_card.hp}
-          energy={current_card.energy}
-          attack={current_card.attack}
-          defense={current_card.defense}
-          price={current_card.price}
+          {...current_card}
         ></CardSimpleDisplay>
       );
       break;
