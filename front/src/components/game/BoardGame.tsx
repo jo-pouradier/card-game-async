b958ed00687f509569fa87ad5ba0b9025da7691b
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-11 17:28:11 +0100

    front: init board game

---
+++ front/src/components/game/BoardGame.tsx
@@ -0,0 +1,13 @@
+import { Container, Divider } from "@mui/material";
+
+const  BoardGame = () => {
+    
+    return (
+        <Container>
+            <Divider textAlign="center"> VS </Divider>
+        </Container>
+    )
+
+}
+
+export default BoardGame;
