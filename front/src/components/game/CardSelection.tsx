5e9576bb070661657b00f27d90d8f7eee182c4a2
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-11 16:45:03 +0100

    front: add search game after complete selection

--- front/src/components/game/CardSelection.tsx
+++ front/src/components/game/CardSelection.tsx
@@ -1,7 +1,8 @@
-import { Container, Grid2, Typography } from "@mui/material";
+import { Button, Container, Grid2, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getCards } from "../../api/user";
-import { useAppSelector } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { addNotification } from "../../slices/notificationSlice";
 import { selectUser } from "../../slices/userSlice";
 import ICard from "../../types/ICard";
 import CardSelect from "../card/CardSelectPlay";
@@ -15,6 +16,7 @@ const CardSelection = () => {
   const [selectedCard, setSelectedCard] = useState<number[]>([]);
   const [cards, setCards] = useState<ICard[]>([]);
   const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     let active = true;
@@ -39,45 +41,55 @@ const CardSelection = () => {
     }
   };
 
+  const searchGame = () => {
+    console.log('Search Game');
+    dispatch(addNotification({id: 0, message: 'Searching for game', severity: 'info'}));
+    // TODO: Implement search game
+  }
+
   return (
     <>
-    <Container style={{ height: "50vh", overflowY: "scroll"}}>
-      <Grid2 container spacing={1} direction={"row"}>
-        {cards
-          .filter((card) => card.id !== user.id)
-          .map(
-            (
-              card,
-              index, // TODO change for ===
-            ) => (
-              <Grid2 size={3} key={index}>
-                <CardSelect
-                  {...card}
-                  isSelected={selectedCard.includes(card.id)}
-                  onSelection={handleCardSelection}
-                />
-              </Grid2>
-            ),
+      <Container style={{ height: "50vh", overflowY: "scroll" }}>
+        <Grid2 container spacing={1} direction={"row"}>
+          {cards
+            .filter((card) => card.id !== user.id)
+            .map(
+              (
+                card,
+                index, // TODO change for ===
+              ) => (
+                <Grid2 size={3} key={index}>
+                  <CardSelect
+                    {...card}
+                    isSelected={selectedCard.includes(card.id)}
+                    onSelection={handleCardSelection}
+                  />
+                </Grid2>
+              ),
+            )}
+        </Grid2>
+      </Container>
+      <Container style={{ marginTop: "2rem" }}>
+        <div style={{display: 'flex'}}>
+          <Typography variant="h4" style={{marginRight: '1rem'}}>
+            Selected Cards ({selectedCard.length}/5)
+          </Typography>
+          {selectedCard.length === 5 && (
+            <Button variant="contained" color="primary" onClick={() => searchGame()}>
+              Play
+            </Button>
           )}
-      </Grid2>
-    </Container>
-    <Container style={{marginTop: "2rem"}}>
-      <Typography variant="h4">Selected Cards ({selectedCard.length}/5)</Typography>
-      <Grid2 container spacing={1} direction={"row"}>
-        {cards
-          .filter((card) => selectedCard.includes(card.id))
-          .map(
-            (
-              card,
-              index,
-            ) => (
-              <Grid2 size={3} key={index}>
+        </div>
+        <Grid2 container spacing={1} direction={"row"}>
+          {cards
+            .filter((card) => selectedCard.includes(card.id))
+            .map((card, index) => (
+              <Grid2 size={2.3} key={index}>
                 <CardShortDisplay {...card} />
               </Grid2>
-            ),
-          )}
-      </Grid2>
-    </Container>
+            ))}
+        </Grid2>
+      </Container>
     </>
   );
 };
