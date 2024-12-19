ce87dfb646727a9e5c5143aac7455d8ac4b00503
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2024-12-19 18:31:57 +0100

    finished it listen to topics + insert log in db

--- backend/Logger/src/main/java/com/cpe/springboot/model/LoggerModel.java
+++ backend/Logger/src/main/java/com/cpe/springboot/model/LoggerModel.java
@@ -2,10 +2,9 @@ package com.cpe.springboot.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
@@ -14,17 +13,31 @@ public class LoggerModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private long timestamp;
-    private String log;
-    private String queue;
 
-    public LoggerModel(long timestamp,String log,String queue) {
+    private long timestamp;
+
+    @Getter
+    @Setter
+    @Column(length = 1000)
+    private String text;
+    private String queue;
+    private String type;
+
+
+    public LoggerModel(long timestamp, String text, String queue, String type) {
         this.timestamp = timestamp;
-        this.log = log;
+        this.text = text;
         this.queue = queue;
+        this.type = type;
     }
 
     public LoggerModel() {
 
     }
+
+    @Override
+    public String toString() {
+        return "timestamp:" + timestamp + " text:" + text + " queue:" + queue + " type:" + type;
+    }
+
 }
